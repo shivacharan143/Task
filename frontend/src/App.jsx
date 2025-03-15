b33d6da6a9316b8e640faab38018a49b98ea0c7a
shivacharan143 <vsc143341@gmail>
2025-03-15 18:35:09 +0530

    vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,79 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import TaskList from "./components/TaskList";
-// import axios from "axios";
-// import AddTask from "./components/AddTask";
-// import AuthForm from "./components/AuthForm";
-// import ProtectedRoute from "./components/ProtectedRoute";
-
-// const App = () => {
-//   const [token, setToken] = useState(localStorage.getItem("token"));
-//   const [tasks, setTasks] = useState([]);
-  
- 
-
-//   // Fetch tasks from the backend
-//   const fetchTasks = async () => {
-//     try {
-//       const response = await axios.get("http://localhost:5000/api/tasks", {
-//         headers: { Authorization: `Bearer ${token}` },
-//       });
-//       setTasks(response.data);
-//     } catch (error) {
-//       console.error("Failed to fetch tasks:", error);
-//     }
-//   };
-
-//   // Fetch tasks when the component mounts
-//   useEffect(() => {
-//     if (token) {
-//       fetchTasks();
-//     }
-//   }, [token]);
-
-
-
-//   return (
-//     <Router>
-       
-//       <div className="container mx-auto p-4 ">
-//         <Routes>
-//           <Route path="/auth" element={<AuthForm setToken={setToken} />} />
-//           <Route
-//             path="/"
-//             element={
-//               <ProtectedRoute token={token}>
-//                 <TaskList tasks={tasks} fetchTasks={fetchTasks} />
-//               </ProtectedRoute>
-//             }
-//           />
-//           <Route
-//             path="/add"
-//             element={
-//               <ProtectedRoute token={token}>
-//                 <AddTask fetchTasks={fetchTasks} />
-//               </ProtectedRoute>
-//             }
-//           />
-//         </Routes>
-       
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TaskList from "./components/TaskList";
@@ -144,3 +68,14 @@ const App = () => {
 };
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
